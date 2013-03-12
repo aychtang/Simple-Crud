26cee669e9242eab6e5343046f6e93d89246fe5f
Hack Reactor Students <accounts@hackreactor.com>
2013-03-12 16:00:09 -0700

    init commit

---
+++ shared.js
@@ -0,0 +1,1 @@
+  var Things = new Meteor.Collection('Things');
---
+++ todo.js
@@ -0,0 +1,37 @@
+if (Meteor.isClient) {
+
+  Template.container.toDo = function () {
+    return Things.find({});
+  };
+
+  Template.thing.editing = function () {
+    if(Session.get('editing') === this._id) return true;
+    else if (Session.get('editing') === null) return false;
+  };
+
+  Template.header.events = {
+    'click .sub' : function () {
+      var item = $('.todo').val();
+      if (item.length !== 0) Things.insert({todo: item});
+    }
+  };
+
+  Template.thing.events ={
+    'click .destroy' : function () {
+      Things.remove(this);
+    },
+
+    'click .listItem' : function () {
+      console.log(this);
+      Session.set('editing', this._id);
+    },
+
+    'click .subUpdate' : function (event) {
+      Things.update({_id: this._id}, {todo: $('.update').val()});
+      Session.set('editing', null);
+      event.stopPropagation();
+    }
+  };
+
+}
+
